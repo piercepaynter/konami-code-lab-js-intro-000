4451394af86ad6dcc32fa268c05113c9fbe2c5b5
Pierce Paynter <paynterpierce@gmail.com>
2020-06-29 01:26:50 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -18,13 +18,13 @@ function init() {
   document.body.addEventListener("keydown", (event) => {
   // Now, how can we check for which specific key was pressed?
   const key = e.key;
- 
+
   if (key === alphabet[index]) {
     index++;
- 
+
     if (index === alphabet.length) {
       alert("Hurray!");
- 
+
       index = 0;
     }
   } else {
