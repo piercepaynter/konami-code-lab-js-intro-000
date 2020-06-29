158528a3917a9e0515be17857c28fb5dbb102e62
Pierce Paynter <paynterpierce@gmail.com>
2020-06-29 01:39:28 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -17,7 +17,7 @@ function init() {
   // Attaching an keydown event listener to document.body
   //document.body.addEventListener("keydown", function(e) {
   const body = document.getElementById('body');
-  
+
   body.addEventListener("keydown", function(e) {
   // Now, how can we check for which specific key was pressed?
   const key = e.key;
